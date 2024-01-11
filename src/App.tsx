c9d3996be03898786779163be9c8f7f8153ab8bf
シン・ドングン <>
2024-01-11 18:30:28 +0900

    메뉴 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import Login from 'pages/Login';
 import { Route, Routes } from 'react-router-dom';
 import Initial from 'pages/Initial';
+import Menu from 'pages/Menu';
+import TodoList from 'pages/TodoList';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path='' element={<Initial />}/>
         <Route path='/login' element={<Login />}/>
+        <Route path="/menu" element={<Menu />} />
+        <Route path='/todoList' element={<TodoList />}/>
       </Routes>
     </div>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,5 @@
-import { Box } from "@mui/material"
 import "./Login.css"
 import React from 'react'
-import { relative } from "path"
 import Auth from "components/login/Auth"
 
 export default function login() {
